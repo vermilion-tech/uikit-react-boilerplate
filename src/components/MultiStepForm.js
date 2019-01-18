9f61ec9ec94b0b4a9bd99efd87be96ea2a7459dd
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-18 10:50:10 -0700

    MultiStepForm left/right arrow functionality

--- src/components/MultiStepForm.js
+++ src/components/MultiStepForm.js
@@ -4,15 +4,17 @@ import LoremIpsum from 'lorem-ipsum'
 
 const LeftRightArrows = (props) => (
   <div className='uk-position-bottom-right uk-padding'>
-    <span data-ratio='2' data-uk-icon='chevron-left' />
-    <span data-ratio='2' data-uk-icon='chevron-right' />
+    <a href='#' data-uk-switcher-item='previous' data-uk-icon='icon: chevron-left; ratio: 1.5' />
+    <a href='#' data-uk-switcher-item='next' data-uk-icon='icon: chevron-right; ratio: 1.5' />
   </div>
 )
 
 const FormStep = (props) => (
-  <div>
+  <div className='uk-margin'>
     <h2 className='monet-text-gold'>{props.heading || LoremIpsum().replace('.', '?')}</h2>
-    {props.content || <Lorem seed={props.index} count={1} />}
+    <div>
+      {props.content || <Lorem seed={props.index} count={1} />}
+    </div>
   </div>
 )
 
@@ -20,7 +22,7 @@ const MultiStepForm = (props) => (
   <div className='multistepform uk-box-shadow-medium uk-flex'>
     {/* tabs */}
     <div className=' uk-background-muted uk-width-1-4@s'>
-      <ul className='uk-tab-left uk-margin-remove-bottom' data-uk-tab='connect: #form-switcher'>
+      <ul className='uk-tab uk-tab-left uk-margin-remove-bottom' data-uk-switcher='connect: .form-switcher'>
         <li> <a href='#'>$0,000 - $0,000,000</a> </li>
         <li> <a href='#'>How soon?</a> </li>
         <li> <a href='#'>Years in business</a> </li>
@@ -32,9 +34,14 @@ const MultiStepForm = (props) => (
 
     {/* form content */}
     <div className='uk-background-default uk-padding uk-width-expand uk-position-relative'>
-      <ul id='form-switcher' className='uk-switcher uk-margin'>
+      <ul className='form-switcher uk-switcher'>
         {
-          Array.from({ length: 6}, (_, i) => <li key={i}> <FormStep index={i} /> </li>)
+          Array.from({ length: 6 }, (_, i) => (
+            <li key={i}>
+              <FormStep index={i} />
+              <LeftRightArrows />
+            </li>
+          ))
         }
       </ul>
     </div>
